3d88bdb439102785047ed53e7065657a24e150c9
Derek Stobbe <djstobbe@gmail.com>
2016-04-09 21:56:45 -0600

    Test if OnValidate runs in a build player

---
+++ Assets/OnValidate Test/ValidatedThing.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ValidatedThing : MonoBehaviour
+{
+	public int refVal;
+	int i = 0;
+
+	void OnValidate ()
+	{
+		i = refVal;
+	}
+
+	void OnGUI ()
+	{
+		GUI.Label (new Rect (10, 10, 100, 200), string.Format ("refVal is {0}; i is {1}", refVal, i));
+	}
+}
